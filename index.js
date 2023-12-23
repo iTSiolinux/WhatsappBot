2544df5b20c7918528ebcda99d4087161b99fb68
Isaac Aber <79910206+iTSiolinux@users.noreply.github.com>
2023-12-23 17:37:33 +0000

    Update 6 files

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
+const { Client } = require("whatsapp-web.js");
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
-
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+// setting up the client
+const client = new Client({
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("qr", (qr) => {
+  console.log(qr);
+  //  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", (message) => {
+  if (message.body === "!ping") {
+    message.reply("pong");
+  }
 });
 
 client.initialize();
